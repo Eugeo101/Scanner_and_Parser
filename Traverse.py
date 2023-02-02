2639c9c9d458179cf5c7842b602fa88f0cd5edbd
AdhamMohamedSaber <85443892+AdhamMohamedSaber@users.noreply.github.com>
2023-02-02 15:45:04 +0200

    Tree construction and traversal.

--- Traverse.py
+++ Traverse.py
@@ -1,1 +1,78 @@
+class Traverse:
+    def __init__(self, dot):
+        self.i = 0
+        self.dot = dot
+    # Traverse
+    def traverse(self, node):
+        # base 1 return
+        if node == None:
+            return None
+        root_key = node.root_key # name of node
+        root_value = node.root_value # value of node
+        root_shape = node.shape # shape of node
+        # base 2
+        if root_key in ['id', 'const']:
+            # print - draw_node
+            var = f"node_{root_key}_{self.i}"
+            self.i += 1
+            self.dot.node(var, f"{root_key}\n({root_value})", {'color': 'darkseagreen2', 'style': 'filled', 'shape': 'oval'})
+            return var
 
+        # determine node by root_key
+        if root_key == 'op':
+            # print - draw_node
+            var = f"node_op_{self.i}"
+            self.i += 1
+            self.dot.node(var, f"{root_key}\n({root_value})", {'color': 'darkseagreen2', 'style': 'filled', 'shape': 'oval'})
+
+            var_left = self.traverse(node.left)
+            if not (var_left == None):
+                self.dot.edge(var, var_left)
+
+            var_right = self.traverse(node.right)
+            if not(var_right == None):
+                self.dot.edge(var, var_right)
+
+            # secret weapon
+            with self.dot.subgraph() as s:
+                s.attr(rank='same')
+                s.node(var_left)
+                s.node(var_right)
+            self.dot.edge(var_left, var_right, color= 'white')
+
+        elif root_key == 'if':
+            # print - draw_node
+            var = f"node_if_{self.i}"
+            self.i += 1
+            self.dot.node(var, f"{root_key}", {'color': 'darkseagreen2', 'style': 'filled', 'shape': 'square'})
+
+            var_test = self.traverse(node.test)
+            if not(var_test == None):
+                self.dot.edge(var, var_test)
+            var_then = self.traverse(node.then)
+            if not(var_then == None):
+                self.dot.edge(var, var_then)
+            var_Else = self.traverse(node.Else)
+            if not(var_Else == None):
+                self.dot.edge(var, var_Else)
+
+            # secret weapon
+            with self.dot.subgraph() as s:
+                s.attr(rank='same')
+                s.node(var_test)
+                s.node(var_then)
+                if not (var_Else == None):
+                    s.node(var_Else)
+            self.dot.edge(var_test, var_then, color='white')
+            if not (var_Else == None):
+                self.dot.edge(var_then, var_Else, color='white')
+
+            # right_flag = True
+            var_next = self.traverse(node.next)
+            if not (var_next == None):
+                with self.dot.subgraph() as s:
+                    s.attr(rank='same')
+                    s.node(var)
+                    s.node(var_next)
+                    s.attr(rank='same')
+                self.dot.edge(var, var_next)
--- utils.py
+++ utils.py
@@ -1,1 +1,46 @@
+class makeOpNode:  # op so have left/right
 
+    def __init__(self, root_node):
+        self.root_key = 'op'
+        self.root_value = root_node['tokenvalue']
+        self.shape = 'oval'
+
+    def left_child(self, node):
+        self.left = node
+
+    def right_child(self, node):
+        self.right = node
+
+
+class make_Id_Const_Node:  # id/const so have no childs
+
+    def __init__(self, root_node):
+        temp = 'const'
+        if root_node['tokentype'] == 'IDENTIFIER':
+            temp = 'id'
+
+        self.root_key = temp
+        self.root_value = root_node['tokenvalue']
+        self.shape = 'oval'
+
+
+class make_if_Node:  # if have 3 childs
+
+    def __init__(self, root_node):
+        self.root_key = 'if'
+        self.root_value = root_node['tokenvalue']
+        self.shape = 'square'
+        self.Else = None
+        self.next = None
+
+    def test_child(self, node):
+        self.test = node
+
+    def then_child(self, node):
+        self.then = node
+
+    def else_child(self, node):
+        self.Else = node
+
+    def next_child(self, node):
+        self.next = node
